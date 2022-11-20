a4c24e3ad57b3d439e1a22cdf33c775e78e03a1e
Ayuska Singh <114468408+Ayuska-Singh@users.noreply.github.com>
2022-11-20 18:53:50 +0530

    Add files via upload

--- traverse.c
+++ traverse.c
@@ -3,9 +3,9 @@ int main()
 {
     int arr[20];
     int n,i;
-    printf("enter the size of the array");
+    printf("enter the size of the array\n");
     scanf("%d",&n);
-    printf("enter the elements of the array");
+    printf("enter the elements of the array\n");
     for(i=0;i<n;i++)
     scanf("%d",&arr[i]);
     printf("elements of the array are\n");
